ac583e91967c85cbca676ef6b95a44013b33d38a
Daniel Naber <naber@danielnaber.de>
2015-04-15 17:39:02 +0200

    remove items from list of exceptions that are not needed anymore

--- src/test/java/de/danielnaber/jwordsplitter/GermanFullDictWordSplitterTest.java
+++ src/test/java/de/danielnaber/jwordsplitter/GermanFullDictWordSplitterTest.java
@@ -15,9 +15,12 @@
  */
 package de.danielnaber.jwordsplitter;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -28,6 +31,37 @@ public class GermanFullDictWordSplitterTest {
     public void testSplit() throws IOException {
         GermanWordSplitter splitter = new GermanWordSplitter(true);
         assertThat(splitter.splitWord("Hausrichten").toString(), is("[Hausrichten]"));  // 'richten' is in removals.txt
+    }
+
+    @Ignore("use to manually clean up exceptionsGerman.txt")
+    @Test
+    public void testExceptions() throws IOException {
+        GermanWordSplitter splitter = new GermanWordSplitter(true);
+        String filename = "src/main/resources/de/danielnaber/jwordsplitter/exceptionsGerman.txt.copy";
+        try (Scanner s = new Scanner(new File(filename))) {
+            int fail = 0;
+            int okay = 0;
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                if (line.startsWith("#")) {
+                    System.out.println(line);
+                    continue;
+                }
+                String input = line.replace("|" , "");
+                String expected = "[" + line.replace("|", ", ") + "]";
+                String result = splitter.splitWord(input).toString();
+                if (!result.equals(expected)) {
+                    //System.out.println(input + " -> " + result + ", expected " + expected);
+                    System.out.println(line);
+                    fail++;
+                } else {
+                    //System.out.println("OK: " + input + " -> " + result);
+                    okay++;
+                }
+            }
+            System.out.println("Okay: " + okay);
+            System.out.println("Fail: " + fail);
+        }
     }
 
 }
